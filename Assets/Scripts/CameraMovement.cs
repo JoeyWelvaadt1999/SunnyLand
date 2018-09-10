33ae38c6e9fa67447ede1f14c8b0c49894cf9ef8
Unknown <kkcbart@live.nl>
2018-09-10 22:27:16 +0200

    Auto stash before merge of "master" and "origin/master"
    Added CameraMovement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    
+    private float _smoothTimeX = 0.2f;
+    private float _smoothTimeY = 0.2f;
+
+    private Vector2 _velocity = Vector2.zero;
+
+    private GameObject _target;
+
+	// Use this for initialization
+	void Start () {
+        _target = GameObject.FindGameObjectWithTag(Constants.PLAYERTAG);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, _target.transform.position.x, ref _velocity.x, _smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, _target.transform.position.y, ref _velocity.y, _smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
