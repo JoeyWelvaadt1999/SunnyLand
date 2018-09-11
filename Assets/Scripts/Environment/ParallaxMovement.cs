5243d30e3f407c7bcd349e84ff21609885ba054e
Joey1999 <joeywelvaadt@gmail.com>
2018-09-11 08:04:30 +0200

    Started game scene
    11/09

---
+++ Assets/Scripts/Environment/ParallaxMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMovement : MonoBehaviour {
+    [SerializeField] private float _speed;
+    private Vector3 _startPos;
+	// Use this for initialization
+	void Start () {
+        _startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 tPos = transform.position;
+        tPos.x -= x * Time.deltaTime * _speed;
+        tPos.y = Camera.main.transform.position.y + _startPos.y;
+        transform.position = tPos;
+
+	}
+}
